312b2dd12cc486fd5d6ea4ff1f84c8aa320def31
pwiener <patryk.wiener@siroccomobile.com>
2021-11-27 11:23:21 +0100

    pep8 notes

---
+++ pep8/sample.py
@@ -0,0 +1,67 @@
+# operator line break
+income = (gross_wages
+          + taxable_interest
+          + (dividends - qualified_dividends)
+          - ira_deduction
+          - student_loan_interest)
+
+PI = 3.14
+
+
+def func1():
+    pass
+
+
+def func2():
+    pass
+
+
+class A:
+    def method1(self):
+        pass
+
+    def method2(self):
+        pass
+
+def func4():
+    if True:
+        if True:
+            pass
+
+    else:
+        pass
+
+def func3(a, b, c):
+    """
+    This func robi cos tam
+
+    :param a: asdfsf
+    :param b: fdsfds
+    :param c: dsfds
+    :return:
+    """
+    if True:
+        if True:
+            func4()
+    elif False
+
+
+l = 0
+I = 0
+O = 0
+
+
+x = 1
+y = 2
+long_variablelong_variablelong = 3
+
+
+
+if __name__ == '__main__':
+    for _ in range(79):
+        print(1, end='')
+    a = (
+        '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111no'
+        'walinijkanowalinijkanowalinijkanowalinijkanowalinijkanowalinijkanowalinijkanowalinijkanowalinijkanowalinijkano'
+        'walinijkanowalinijkanowalinijkanowalinijka'
+    )
